92a7f71bdc859bd1143897936f9cad24f640578e
LuisHernandez1389 <99280352+LuisHernandez1389@users.noreply.github.com>
2023-12-06 02:35:21 -0700

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,28 @@
+const self = this;
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          './index.html',
+          './pages/eventos.html',
+          './pages/ordenes.html',
+          './pages/usuarios.html',
+          
+          
+
+
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
